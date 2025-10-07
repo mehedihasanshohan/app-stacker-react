2a88eb10100daac02c869eee02705e1e3b46b63c
mehedi hasan <mhshohan04@gmail.com>
2025-10-08 00:00:03 +0600

    added navbar section

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router'
 import errorImg from '/assets/error-404.png'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const Errorpage = () => {
   return (
+    <div>
+    <Navbar></Navbar>
     <div className='bg-[#CCE1F7] min-h-screen flex flex-col justify-center items-center gap-6'>
       <img src={errorImg} alt="" />
       <h2 className='text-black font-bold text-2xl'>Oops! page not found!</h2>
       <p className='text-gray-500 font-normal '>The page you are looking is not available</p>
       <Link to={-1} className='btn btn-info'>Go Back!</Link>
     </div>
-
+    <Footer></Footer>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router'
+import icon from '/assets/logo.png'
+import { FaGithub } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu  text-lg dropdown-content bg-white text-black rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><Link>Home</Link></li>
+        <li><Link>Apps</Link></li>
+        <li><Link>Installations</Link></li>
+      </ul>
+    </div>
+     <img src={icon} className='h-8 w-8 ml-4 mr-2' alt="logo" />
+     <h2 className='text-2xl font-bold text-purple-800'>
+     <Link to={'/'}>App Stacker</Link>
+    </h2>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal text-lg font-semibold px-1">
+      <li><Link  to={'/'}>Home</Link></li>
+      <li><Link to={'/apps'}>Apps</Link></li>
+      <li><Link to={'/installations'}>Installations</Link></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <button
+      onClick={() => window.open("https://github.com/mehedihasanshohan", "_blank")}
+      className="btn relative overflow-hidden rounded-xl
+                  bg-gradient-to-r from-[#632EE3] to-[#9F63F2]
+                text-white border-none backdrop-blur-lg flex
+                  items-center gap-2 px-5 py-2 hover:from-[#9F63F2] hover:to-[#632EE3] mr-4 ]">
+    <FaGithub className="text-lg" />
+    <Link to="/" className="font-medium">Contribution</Link>
+  </button>
+
+  </div>
+</div>
   )
 }
 
