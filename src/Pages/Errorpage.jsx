ba3f2a95a89eed710235c72c79cd02a63c966907
mehedi hasan <mhshohan04@gmail.com>
2025-10-07 23:34:08 +0600

    added 404 page

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
+import errorImg from '/assets/error-404.png'
 
 const Errorpage = () => {
   return (
-    <div>Errorpage</div>
+    <div className='bg-[#CCE1F7] min-h-screen flex flex-col justify-center items-center gap-6'>
+      <img src={errorImg} alt="" />
+      <h2 className='text-black font-bold text-2xl'>Oops! page not found!</h2>
+      <p className='text-gray-500 font-normal '>The page you are looking is not available</p>
+      <Link to={-1} className='btn btn-info'>Go Back!</Link>
+    </div>
+
   )
 }
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import Home from './../Pages/Home';
+import MainLayOut from './../Layouts/MainLayOut';
+import Errorpage from "../Pages/Errorpage";
+import Banner from "../components/Banner/Banner";
+import AppDetails from "../Pages/AppDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: '/apps',
-    element: <div> apps</div>
+    element: <MainLayOut></MainLayOut>,
+    errorElement: <Errorpage></Errorpage>,
+    children: [
+      { index: true, element: <Banner></Banner>},
+      { path: '/details', element: <AppDetails></AppDetails>}
+    ]
   }
 ]);
 
